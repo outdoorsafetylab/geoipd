d4b55c6541fb48e2d0366797f201ef1d65f7999c
Cedric Shih <cedric.shih@gmail.com>
2022-09-04 23:30:02 +0800

    wip

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,7 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "geoipd",
+	Use:   "geoip",
 	Short: "GeoIP2 server",
 }
 
